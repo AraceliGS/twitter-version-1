1007eb8ac13d6a5128103c5e7b18e959451758fd
AraceliGS <aragutarras@gmail.com>
2017-11-18 11:42:04 -0500

    añadiendo estilos a section y a los divs que contiene, añadiendo ademas el evento (on)click a la etiqueta button

---
+++ js/app.js
@@ -0,0 +1,16 @@
+// Para que estos eventos ocurran cuando la página haya cargado completamente, usó el evento load
+
+window.addEventListener('load', function() {
+  var button = document.querySelector('button');
+  var textarea = document.querySelector('textarea');  
+
+  button.addEventListener('click', function() {
+    var sectionDivs = document.getElementsByTagName('section')[0];
+    var div = document.createElement('div');
+    var message = textarea.value;  
+    var text = document.createTextNode(message);
+    div.appendChild(text);    
+    sectionDivs.appendChild(div);
+    div.classList.add('divText');
+  });
+});
