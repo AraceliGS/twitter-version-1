889639747529aff37caa954ecf7111bec2b67ee1
AraceliGS <aragutarras@gmail.com>
2017-11-25 02:07:07 -0500

    añadiendo cambios al nombre de la variable

--- js/app.js
+++ js/app.js
@@ -2,12 +2,12 @@
 
 window.addEventListener('load', function() { 
   var button = document.querySelector('button'); // Utilizo este método para que obtenga el primer valor cuyo selector de etiqueta sea button(botón).
-  var textarea = document.querySelector('textarea'); // Utilizo este método para que obtenga el primer valor cuyo selector de etiqueta sea textarea(área de texto).
+  var textArea = document.querySelector('textarea'); // Utilizo este método para que obtenga el primer valor cuyo selector de etiqueta sea textarea(área de texto).
   // Estableciendo el evento click al elemento button
   button.addEventListener('click', function() {
     var sectionDivs = document.getElementsByTagName('section')[0]; // Seleccionando a la sección que contendra  alos divs con los comentarios.
     var div = document.createElement('div'); // Creando un div.
-    var message = textarea.value; // Encapsulando lo escrito en el textarea en una variable.
+    var message = textArea.value; // Encapsulando lo escrito en el textarea en una variable.
     var text = document.createTextNode(message); // Creando un nodo de texto cuyo contenido es el valor de la variable message.
     div.appendChild(text); // Uniendo al nodo de texto 'text' con el nodo de tipo elemento div.   
     sectionDivs.appendChild(div); // Uniendo al nodo de tipo elemento div con el nodo de tipo elemento section, para que el div sea contenido dentro de la sección.
